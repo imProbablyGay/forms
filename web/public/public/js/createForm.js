17890b49614bd704c6ee0d7b1ed8afa66b691c44
imProbablyGay <temadoron9@gmail.com>
2023-01-18 16:48:15 +0200

    yes ll

--- web/public/public/js/createForm.js
+++ web/public/public/js/createForm.js
@@ -77,7 +77,7 @@ function initUploadFormModal(id = 'uploadForm') {
     });
 }
 
-//text count
+//text count 33
 function displayTextCount(lengthAfter = null, e = null, editor = null) {
     const limit = 10;
     const keysToExclude = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Backspace'];
