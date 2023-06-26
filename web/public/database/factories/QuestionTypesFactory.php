ba9827f216529ce78918ea9dbd6d421b0902ff93
imProbablyGay <temadoron9@gmail.com>
2023-06-26 12:48:15 +0300

    factory

---
+++ web/public/database/factories/QuestionTypesFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\QuestionTypes as DB;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\QuestionTypes>
+ */
+class QuestionTypesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+        ];
+    }
+}
